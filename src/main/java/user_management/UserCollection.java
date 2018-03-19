d7933de36b100e46981518a0a0c93a72f9d2dc59
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-03-19 14:33:52 -0400

    why regex, why

--- src/main/java/user_management/UserCollection.java
+++ src/main/java/user_management/UserCollection.java
@@ -1,5 +1,6 @@
 package user_management;
 
+import user_management.security.Password;
 import user_management.security.UserAuthenticationFailedException;
 import user_management.validation.EmailNotAvailableException;
 import user_management.validation.InvalidEmailException;
@@ -7,6 +8,7 @@ import user_management.validation.PasswordTooSimpleException;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class UserCollection extends ArrayList<User> {
@@ -44,7 +46,21 @@ public class UserCollection extends ArrayList<User> {
     }
 
     public int createUser(String name, String email, String password)
-            throws EmailNotAvailableException, InvalidEmailException, PasswordTooSimpleException {
-
+            throws PasswordTooSimpleException, EmailNotAvailableException, InvalidEmailException {
+        Iterator<User> iterator = this.iterator();
+        User user = iterator.next();
+        if (user.getEmail().equals(email)){
+            throw new EmailNotAvailableException("This email is taken by another user.");
+        }
+        if (!email.matches("[A-Z0-9._%!&#$()*+-]+")){
+            throw new InvalidEmailException("This email sucks");
+        }
+        if (!password.matches("(?=.*[a-z])") || !password.matches("(?=.*[A-Z])") ||
+                !password.matches("(?=.*[0-9])") || !password.matches("(?=.[!@#\\$%\\^&])") ||
+                !password.matches("(?=.{8,})")){
+            throw new PasswordTooSimpleException("This password sucks");
+        }
+        return 0;
     }
+
 }
