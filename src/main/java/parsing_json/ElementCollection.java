058994bf62f0d89a5337611bdbd3e1fbc9484242
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-03-19 09:41:15 -0400

    element test pass

--- src/main/java/parsing_json/ElementCollection.java
+++ src/main/java/parsing_json/ElementCollection.java
@@ -1,6 +1,8 @@
 package parsing_json;
 
-public class ElementCollection {
+import java.util.ArrayList;
+
+public class ElementCollection extends ArrayList<Element> {
 
     public Element findByAtomicNumber(int atomic_number) {
         return null;
--- src/main/java/parsing_json/ElementCollectionInitializer.java
+++ src/main/java/parsing_json/ElementCollectionInitializer.java
@@ -1,7 +1,22 @@
 package parsing_json;
 
+import com.google.gson.Gson;
+
+import javax.xml.transform.Result;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ElementCollectionInitializer {
     public static ElementCollection generate() {
-        return null;
+        Gson gson = new Gson();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("periodic_table.json"));
+            Result result = gson.fromJson(br, Result.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
