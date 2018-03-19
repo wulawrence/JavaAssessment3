18573fccc29063d834d9f8090845ab32d0eb616a
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-03-19 11:40:36 -0400

    setaccessible is evil

--- src/main/java/parsing_json/ElementCollection.java
+++ src/main/java/parsing_json/ElementCollection.java
@@ -2,12 +2,20 @@ package parsing_json;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ElementCollection extends ArrayList<Element> {
 
-    private ArrayList<Element> elements = ElementCollectionInitializer.generate();
+
     public Element findByAtomicNumber(int atomic_number) {
-        for (Element element: elements){
+//        for (Element element: elements){
+//            if (element.getNumber() == atomic_number){
+//                return element;
+//            }
+//        }
+        Iterator<Element> iterator = this.iterator();
+        while (iterator.hasNext()){
+            Element element = iterator.next();
             if (element.getNumber() == atomic_number){
                 return element;
             }
@@ -16,7 +24,14 @@ public class ElementCollection extends ArrayList<Element> {
     }
 
     public Element findByName(String name) {
-        for (Element element: elements){
+//        for (Element element: elements){
+//            if (element.getName().equalsIgnoreCase(name)){
+//                return element;
+//            }
+//        }
+        Iterator<Element> iterator = this.iterator();
+        while (iterator.hasNext()){
+            Element element = iterator.next();
             if (element.getName().equalsIgnoreCase(name)){
                 return element;
             }
@@ -25,12 +40,31 @@ public class ElementCollection extends ArrayList<Element> {
     }
 
     public ElementCollection where(String fieldName, Object value) {
+//        ElementCollection emptyCollection = new ElementCollection();
+//        for (int i = 0; i < elements.size(); i++){
+//            try {
+//                Field matchingField = elements.get(i).getClass().getDeclaredField(fieldName);
+//                if (matchingField.equals(value)){
+//                    elements.add(elements.get(i));
+//                }
+//            } catch (NoSuchFieldException e) {
+//                return emptyCollection;
+//            }
+//        }
+//        return emptyCollection;
         ElementCollection emptyCollection = new ElementCollection();
-        for (int i = 0; i < elements.size(); i++){
+        Iterator<Element> iterator = this.iterator();
+        while (iterator.hasNext()) {
+            Element element = iterator.next();
             try {
-                Field matchingField = elements.get(i).getClass().getDeclaredField(fieldName);
-                if (matchingField.equals(value)){
-                    elements.add(elements.get(i));
+                Field matchingField = element.getClass().getDeclaredField(fieldName);
+                matchingField.setAccessible(true);
+                try {
+                    if (matchingField.get(element).equals(value)) {
+                        emptyCollection.add(element);
+                    }
+                } catch (IllegalAccessException ie){
+                    return emptyCollection;
                 }
             } catch (NoSuchFieldException e) {
                 return emptyCollection;
--- src/main/java/user_management/UserCollection.java
+++ src/main/java/user_management/UserCollection.java
@@ -1,14 +1,29 @@
 package user_management;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class UserCollection extends ArrayList<User> {
 
     public User findById(int id) {
+        Iterator<User> iterator = this.iterator();
+        while(iterator.hasNext()){
+            User user = iterator.next();
+            if (user.getId() == id){
+                return user;
+            }
+        }
         return null;
     }
 
     public User findByEmail(String email) {
+        Iterator<User> iterator = this.iterator();
+        while(iterator.hasNext()){
+            User user = iterator.next();
+            if (user.getEmail().equalsIgnoreCase(email)){
+                return user;
+            }
+        }
         return null;
     }
 
